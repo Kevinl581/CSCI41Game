a36eed5a89b945c501af410dd9024acf858afca7
smallwoodRaine <rainesmallwood@gmail.com>
2018-04-09 06:58:06 -0700

    initialized classes for bomb, player and hostages

---
+++ bombrush.h
@@ -0,0 +1,10 @@
+#include<ncurses.h>
+class bomb;
+class hostage;
+class player;
+const unsigned char bmb = 'B';
+const unsigned char htge = 'H';
+const unsigned char pyr = 'P';
+int xdim, ydim; //world dimensions
+
+
