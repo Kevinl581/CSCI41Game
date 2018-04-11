f31b8d2afcdb4e957e252867380ed8ab9af295ee
smallwoodRaine <rainesmallwood@gmail.com>
2018-04-11 12:45:38 -0700

    added changes

--- main.cc
+++ main.cc
@@ -4,8 +4,90 @@
 #include<cmath>
 #include"bombrush.h"
 using namespace std;
+bool wall[100][100];
+void ulose(){
+    
+}
 void printWorld(){
+    for(int i=0; i<100; i++)
+        for(int j=0; j<100; j++){
+            if(i==0||i==20){
+                if(j<76){
+                mvaddch(i, j, '_');
+                wall[i][j]=true;
+            }}
+            if(j==0||j==75){
+                if(i<21&&i>0){
+                mvaddch(i, j, '|');
+                wall[i][j]=true;
+                }
+            }
+}
+}
+void init(){
+initscr();
+   clear();
+       noecho();
+       cbreak();
+       keypad(stdscr, TRUE);
+       curs_set(0); }   
+void erase(int y, int x){
+mvaddch(y, x, ' ');
+}
+void gameLoop(char main_char, int row, int col, int ch){
+    if(ch == 'q' || ch =='Q') return;
+        printWorld(); 
+             // Show the main character on the screen
+            mvaddch(row, col, main_char);
+         refresh();
+     
+           for(;;) {
+                  ch = getch();
+            
+                      if(ch == KEY_LEFT) {
+                              erase(row, col);
+                              col = col - 1;
+                              mvaddch(row, col, main_char);
+                              refresh();
+                          }
+                  else if(ch == KEY_RIGHT) {
+                          erase(row, col);
+                          col = col + 1;
+                          mvaddch(row, col, main_char);
+                          refresh();
+                      }
+                 else if(ch == KEY_UP) {
+                          erase(row, col);
+                         row = row - 1;
+                          mvaddch(row, col, main_char);
+                          refresh();
+                      }
+                  else if(ch == KEY_DOWN) {
+                          erase(row, col);
+                          row = row + 1;
+                          mvaddch(row, col, main_char);
+                          refresh();
+                      }
+                  if(wall[row][col]==true)
+                  {
+                      break;
+                  }
+                  else if(ch == 'q' || ch == 'Q') {
+                          break;
+            }
+        }
 }
 int main(){
-    initscr();
+    //define main character symbol and initial position
+    int y = 2, x = 2;
+    char player = 'P';
+    printWorld();
+    //start ncurses
+    init();
+    int input=getch();
+    clear();
+    gameLoop(player, y, x, input);
+    endwin();
+    system("clear");
+    return 0;
 }
