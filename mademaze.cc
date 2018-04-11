db4c2c5a9b876c30d3ebe146cb1046757d4c4d9d
Kevinl581 <kevinlee581@hotmail.com>
2018-04-11 14:00:11 -0700

    Made a random wall generator for maze.

--- mademaze.cc
+++ mademaze.cc
@@ -8,6 +8,26 @@ bool wall[100][100];
 void ulose(){
     
 }
+
+void printMaze(){//Makes a maze within the walls.
+	srand(time(0));
+	for(int i=1; i<19; i++){
+		int xCord = rand()%74+1;//Generates a random number between 1-74.
+	    int yCord = rand()%19+1;//Generates a number between 1-19
+	    if(wall[xCord][yCord] == false){//Checks if there is no wall in the cordinates.
+			mvaddch(yCord, xCord, '|');//Sets that cordinate 
+			wall[yCord][xCord] = true;//Sets the wall at that cordinate to true.
+		}
+	}
+	for(int j=1; j<74; j++){
+ 		int xCord = rand()%74+1;
+    	int yCord = rand()%19+1;
+		if(wall[xCord][yCord] == false)	
+			mvaddch(yCord, xCord, '_');
+			wall[yCord][xCord] = true;
+		}
+}
+
 void printWorld(){
     for(int i=0; i<100; i++)
         for(int j=0; j<100; j++){
@@ -22,7 +42,8 @@ void printWorld(){
                 wall[i][j]=true;
                 }
             }
-}
+	}
+	printMaze();
 }
 void init(){
 initscr();
