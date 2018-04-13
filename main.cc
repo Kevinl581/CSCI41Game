c9a1cc7d96277ba4506056e6dce0c68be1fae86c
Kevinl581 <kevinlee581@hotmail.com>
2018-04-13 14:20:19 -0700

    Made walls, and also made lower wall accessible.

--- main.cc
+++ main.cc
@@ -9,33 +9,40 @@ void ulose(){
     
 }
 
-void printMaze(){//Makes a maze within the walls.
-	srand(time(0));
-	for(int i=1; i<19; i++){
-		int xCord = rand()%74+1;//Generates a random number between 1-74.
-	    int yCord = rand()%19+1;//Generates a number between 1-19
-	    if(wall[xCord][yCord] == false){//Checks if there is no wall in the cordinates.
-			mvaddch(yCord, xCord, '|');//Sets that cordinate 
-			wall[yCord][xCord] = true;//Sets the wall at that cordinate to true.
+void printWalls(){//Makes a maze within the walls.
+	for(int i=1; i<20; i++){
+		for(int j=1; j<10; j++){
+			int xcord = rand()%74+1;
+			int ycord = rand()%19+1;
+	    	if(wall[ycord][xcord] == false){//Checks if there is no wall in the cordinates.
+				mvaddch(ycord, xcord, '#');//Sets that cordinate 
+				wall[ycord][xcord] = true;//Sets the wall at that cordinate to true.
+			}
 		}
 	}
-	for(int j=1; j<74; j++){
- 		int xCord = rand()%74+1;
+	/*for(int j=1; j<74; j++){
+		int xCord = rand()%74+1;
     	int yCord = rand()%19+1;
-		if(wall[xCord][yCord] == false)	
-			mvaddch(yCord, xCord, '_');
+		if(wall[yCord][xCord] == false)	
+			mvaddch(yCord, xCord, '#');
 			wall[yCord][xCord] = true;
-		}
+		}*/
 }
 
 void printWorld(){
     for(int i=0; i<100; i++)
         for(int j=0; j<100; j++){
-            if(i==0||i==20){
+            if(i==0){
                 if(j<76){
                 mvaddch(i, j, '_');
                 wall[i][j]=true;
             }}
+			 if(i==20){
+			    if(j<76){
+			    mvaddch(i, j, '_');
+			    wall[i+1][j]=true;
+			 }}
+			 
             if(j==0||j==75){
                 if(i<21&&i>0){
                 mvaddch(i, j, '|');
@@ -43,7 +50,7 @@ void printWorld(){
                 }
             }
 	}
-	printMaze();
+	printWalls();
 }
 void init(){
 initscr();
@@ -53,7 +60,8 @@ initscr();
        keypad(stdscr, TRUE);
        curs_set(0); }   
 void erase(int y, int x){
-mvaddch(y, x, ' ');
+	if(wall[y] == wall[20]) mvaddch(y, x, '_');
+	else mvaddch(y, x, ' ');
 }
 void gameLoop(char main_char, int row, int col, int ch){
     if(ch == 'q' || ch =='Q') return;
@@ -102,7 +110,8 @@ int main(){
     //define main character symbol and initial position
     int y = 2, x = 2;
     char player = 'P';
-    printWorld();
+	srand(time(NULL));
+    //printWorld();
     //start ncurses
     init();
     int input=getch();
