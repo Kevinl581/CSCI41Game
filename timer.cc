6370c5e3258d86943b061039b395bd1038d1c901
Kevinl581 <kevinlee581@hotmail.com>
2018-04-13 17:05:08 -0700

    Made a little time. Just Compile it to run and edit the min and sec to how much you guys want.

---
+++ timer.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <time.h>
+#include <unistd.h>
+#include <iomanip>
+
+using namespace std;
+
+void printTime() {
+	int min = 0;
+	for (int m = min; m >= 0; m--) {
+		for (int s = 5; s >= 0 ; s--) {
+			if (m == -1) exit(0);
+			cout << '\r' << setw(2) << setfill('0') << m << ":" << setw(2) << setfill('0') << s << flush;
+			usleep(1000000);
+		}	
+	}
+	cout << endl;
+}
+
