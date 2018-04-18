f32600654582432f90e953389c671d14d6f25784
Kevinl581 <kevinlee581@hotmail.com>
2018-04-18 02:10:11 -0700

    Fixed the highscore, and Cleaned up the program a bit.

--- bombrush.h
+++ bombrush.h
@@ -7,84 +7,95 @@
 #include <stdlib.h>
 #include <stdio.h>
 using namespace std;
-class Bomb{
-    private:
-        char bombLet;
-        int bombType;
-    public:
-        Bomb(char newLet, int newType){
-            bombLet=newLet;
-            bombType=newType;
-        }
-        void setBombLet(char newLet){bombLet=newLet;}
-        void setBombType(int newType){bombType=newType;}
-        char getBombLet(){return bombLet;}
-        int getBombType(){return bombType;}
+class Bomb {
+  private:
+	char bombLet;
+	int bombType;
+  public:
+	Bomb(char newLet, int newType) {
+		bombLet = newLet;
+		bombType = newType;
+	}
+	void setBombLet(char newLet) {
+		bombLet = newLet;
+	}
+	void setBombType(int newType) {
+		bombType = newType;
+	}
+	char getBombLet() {
+		return bombLet;
+	}
+	int getBombType() {
+		return bombType;
+	}
 
 };
 int xdim, ydim; //world dimensions
 const int MAX_SCORES = 10;
 void ulose(int end) {
-    clear();
-    //cout << "Game Over.";
-    mvprintw(11, 38, "Game Over.");
-	if (end == 1) { mvprintw(10, 38, "You hit a deadly obstacle!");}
-	if (end == 2) { mvprintw(10, 38, "You cut the wrong wire! BOMB DETONATION.");}
-	
-    refresh();
-	/*
+	clear();
+	//cout << "Game Over.";
+	mvprintw(10, 48, "Game Over.");
+	mvprintw(12, 38, "Press any key to continue.");
+	if (end == 1) {
+		mvprintw(11, 38, "You hit a deadly obstacle!");
+	}
+	if (end == 2) {
+		mvprintw(11, 38, "You cut the wrong wire! BOMB DETONATION.");
+	}
+
 	refresh();
-    cout << "Game Over.";
-    //mvprintw(10, 38, "Game Over.");
-	cout << endl;
-	sleep(3);
-    //refresh();
-	*/
 	getch();
-    clear();
+	clear();
 	//endwin();
-   //system("clear");
-    //exit(1);
+	//system("clear");
+	//exit(1);
 }
 void defuse(Bomb in) {
-    //clear line 12, 76 -- feedback line
-   initscr();
-   //clear();
-   noecho();
-   cbreak();
+	//clear line 12, 76 -- feedback line
+	initscr();
+	//clear();
+	noecho();
+	cbreak();
 
-    
-    mvprintw(12, 76, " ");
-    // bombs need type and letter for defusal. letter will be shown to player. from letter, players must guess the type.
-    char dfuseType;
+
+	mvprintw(12, 76, " ");
+	// bombs need type and letter for defusal. letter will be shown to player. from letter, players must guess the type.
+	char dfuseType;
 	char bType = '0' + in.getBombType();
 
-    // Display bomb color to player
-    //  char bombDisplay = in.bombLet;
-    mvprintw(10, 76, "The bomb is type");
-	if (bType == '1'){mvprintw(10, 93, "A");}
-	if (bType == '2'){mvprintw(10, 93, "B");}
-	if (bType == '3'){mvprintw(10, 93, "C");}
-    //mvprintw(10, 88, "?");
-    mvprintw(11, 76, "Input number of wire to cut.");
-    mvprintw(12, 76, "1 for RED, 2 for BLUE, 3 for GREEN.");
+	// Display bomb color to player
+	//  char bombDisplay = in.bombLet;
+	mvprintw(10, 76, "The bomb is type");
+	if (bType == '1') {
+		mvprintw(10, 93, "A");
+	}
+	if (bType == '2') {
+		mvprintw(10, 93, "B");
+	}
+	if (bType == '3') {
+		mvprintw(10, 93, "C");
+	}
+	//mvprintw(10, 88, "?");
+	mvprintw(11, 76, "Input number of wire to cut.");
+	mvprintw(12, 76, "1 for RED, 2 for BLUE, 3 for GREEN.");
 	refresh();
-    // receive defuse type input
-    dfuseType = getch();
+	// receive defuse type input
+	dfuseType = getch();
 
-    while (dfuseType != '1' && dfuseType != '2' && dfuseType != '3') {
-        mvprintw(13, 76, "Incorrect input.");
-        mvprintw(14, 76, "Enter 1, 2, or 3, like above.");	
-        //cin >> dfuseType;
+	while (dfuseType != '1' && dfuseType != '2' && dfuseType != '3') {
+		mvprintw(13, 76, "Incorrect input.");
+		mvprintw(14, 76, "Enter 1, 2, or 3, like above.");
+		//cin >> dfuseType;
 		dfuseType = getch();
-    }
+	}
 
-    // if defuse type does not match bomb type, game over
-    if (dfuseType != bType) {
-        ulose(2);
-    }
-	
-    // else defuse bomb i.e. remove bomb character, iterate all other bombs down, adjust timer, and add 1 more bomb
+	// if defuse type does not match bomb type, game over
+	if (dfuseType != bType) {
+		ulose(2);
+	}
+
+	// else defuse bomb i.e. remove bomb character, iterate all other bombs down, adjust timer, and add 1 more bomb
 	//clear text if successful
 	mvprintw(10, 76, "                     ");
 	mvprintw(10, 93, "              ");
@@ -94,16 +105,19 @@ void defuse(Bomb in) {
 	mvprintw(14, 76, "                                   ");
 
 	refresh();
-    return;
+	return;
 }
-void highScore(int a){
-     char c[10];
-    sprintf(c, "%d", a);
-   mvprintw(10, 20, "Current High Score is:");
-   mvprintw(10, 45, c);
-    refresh();
-    getch();
-    return;}
+void highScore(int a) {
+	char c[10];
+	sprintf(c, "%d", a);
+	mvprintw(10, 20, "Current High Score is:");
+	mvprintw(10, 45, c);
+	refresh();
+	timeout(3000);
+	getch();
+	timeout(-1);
+	return;
+}
 /*
 class Heap
 {
--- main.cc
+++ main.cc
@@ -15,13 +15,13 @@ void highScore(int a);
 const unsigned char main_char = 'P';
 const unsigned char BOMB = 'B';
 
-void printWalls(){//Makes a maze within the walls.
-	for(int i=1; i<20; i++){
-		for(int j=1; j<10; j++){
-			int xcord = rand()%74+1;
-			int ycord = rand()%19+1;
-	    	if(wall[ycord][xcord] == false){//Checks if there is no wall in the cordinates.
-				mvaddch(ycord, xcord, '#');//Sets that cordinate 
+void printWalls() { //Makes a maze within the walls.
+	for (int i = 1; i < 20; i++) {
+		for (int j = 1; j < 10; j++) {
+			int xcord = rand() % 74 + 1;
+			int ycord = rand() % 19 + 1;
+			if (wall[ycord][xcord] == false) { //Checks if there is no wall in the cordinates.
+				mvaddch(ycord, xcord, '#');//Sets that cordinate
 				wall[ycord][xcord] = true;//Sets the wall at that cordinate to true.
 			}
 		}
@@ -29,158 +29,172 @@ void printWalls(){//Makes a maze within the walls.
 }
 
 
-void printWorld(){
-    for(int i=0; i<100; i++)
-        for(int j=0; j<100; j++){
-            if(i==0){
-                if(j<76){
-                mvaddch(i, j, '_');
-                wall[i][j]=true;
-            }}
-			 if(i==20){
-			    if(j<76){
-			    mvaddch(i, j, '_');
-			    wall[i+1][j]=true;
-			 }}
-			 
-            if(j==0||j==75){
-                if(i<21&&i>0){
-                mvaddch(i, j, '|');
-                wall[i][j]=true;
-                }
+void printWorld() {
+	for (int i = 0; i < 100; i++)
+		for (int j = 0; j < 100; j++) {
+			if (i == 0) {
+				if (j < 76) {
+					mvaddch(i, j, '_');
+					wall[i][j] = true;
+				}
+			}
+			if (i == 20) {
+				if (j < 76) {
+					mvaddch(i, j, '_');
+					wall[i + 1][j] = true;
+				}
+			}
+
+			if (j == 0 || j == 75) {
+				if (i < 21 && i > 0) {
+					mvaddch(i, j, '|');
+					wall[i][j] = true;
+				}
 			}
 		}
 	printWalls();
-    printBombs();
+	printBombs();
 }
-void printBombs(){
-  while(true){
-   int a=rand()%20;
-   int b=rand()%70;
-   if(wall[a][b]!=true){
-       bomb[a][b]=true;
-	   attron(COLOR_PAIR(2));
-       mvaddch(a, b, BOMB);
-	   attroff(COLOR_PAIR(2));
-       return; 
-   }
-  }
+void printBombs() {
+	while (true) {
+		int a = rand() % 20;
+		int b = rand() % 70;
+		if (wall[a][b] != true) {
+			bomb[a][b] = true;
+			attron(COLOR_PAIR(2));
+			mvaddch(a, b, BOMB);
+			attroff(COLOR_PAIR(2));
+			return;
+		}
+	}
+}
+void quit() {
+
+	mvprintw(22, 0, "Are you sure you want to quit? Yes(Y) or No(N)");
+	char ch = getch();
+	if (ch == 'Y' || ch == 'y') {
+		system("clear");
+		printf("Thank you for playing.\n");
+		wait(1);
+		//system("clear");
+		endwin();
+		exit(0);
+	} else {
+		mvprintw(22, 0, "                                                   ");
+		return;
+	}
 }
 
-void init(){
-initscr();
-   clear();
-       noecho();
-       cbreak();
-       keypad(stdscr, TRUE);
-       curs_set(0); }   
-void erase(int y, int x){
-	if(wall[y] == wall[20]) mvaddch(y, x, '_');
+void init() {
+	initscr();
+	clear();
+	noecho();
+	cbreak();
+	keypad(stdscr, TRUE);
+	curs_set(0);
+}
+void erase(int y, int x) {
+	if (wall[y] == wall[20]) mvaddch(y, x, '_');
 	else mvaddch(y, x, ' ');
 }
-int gameLoop(int row, int col, int ch){
-    if(ch == 'q' || ch =='Q') {
-        endwin();
-        system("clear");
-        exit(0);
-    }
-        printWorld(); 
-             // Show the main character on the screen
-            attron(COLOR_PAIR(1));
+int gameLoop(int row, int col, int ch) {
+	printWorld();
+	// Show the main character on the screen
+	attron(COLOR_PAIR(1));
+	mvaddch(row, col, main_char);
+	attroff(COLOR_PAIR(1));
+	refresh();
+
+	int point = 0;
+
+	for (;;) {
+		mvprintw(21, 55, "Points: %d", point);
+		ch = getch();
+
+		if (ch == KEY_LEFT) {
+			erase(row, col);
+			col = col - 1;
+			attron(COLOR_PAIR(1));
 			mvaddch(row, col, main_char);
 			attroff(COLOR_PAIR(1));
-         refresh();
-
-		 int point = 0;
-  		
-         for(;;) {
-			 	mvprintw(21, 55,"Points: %d", point);
-                  ch = getch();
-           			 
-                      if(ch == KEY_LEFT) {
-                              erase(row, col);
-                              col = col - 1;
-                              attron(COLOR_PAIR(1));
-							  mvaddch(row, col, main_char);
-                              attroff(COLOR_PAIR(1));
-							  refresh();
-                          }
-                  else if(ch == KEY_RIGHT) {
-                          erase(row, col);
-                          col = col + 1;
-                          attron(COLOR_PAIR(1));
-						  mvaddch(row, col, main_char);
-                          attroff(COLOR_PAIR(1));
-						  refresh();
-                      }
-                 else if(ch == KEY_UP) {
-                          erase(row, col);
-                         row = row - 1;
-                         attron(COLOR_PAIR(1)); 
-						 mvaddch(row, col, main_char);
-                         attroff(COLOR_PAIR(1));
-						 refresh();
-                      }
-                  else if(ch == KEY_DOWN) {
-                          erase(row, col);
-                          row = row + 1;
-                          attron(COLOR_PAIR(1));
-						  mvaddch(row, col, main_char);
-						  attroff(COLOR_PAIR(1));
-                          refresh();
-                      }
-                  if(bomb[row][col]==true)
-                  {
-					  Bomb a('h',rand()%3+1);
-                      bomb[row][col]=false;
-                      defuse(a);
-					  point++;
-					  printBombs();
-                     break;
-                   }  
-                  if(wall[row][col]==true)
-                  {
-                      ulose(1);
-                      break;
-                  }
-                  else if(ch == 'q' || ch == 'Q') {
-                          break;
-                }
-            }
-         return point;
+			refresh();
+		} else if (ch == KEY_RIGHT) {
+			erase(row, col);
+			col = col + 1;
+			attron(COLOR_PAIR(1));
+			mvaddch(row, col, main_char);
+			attroff(COLOR_PAIR(1));
+			refresh();
+		} else if (ch == KEY_UP) {
+			erase(row, col);
+			row = row - 1;
+			attron(COLOR_PAIR(1));
+			mvaddch(row, col, main_char);
+			attroff(COLOR_PAIR(1));
+			refresh();
+		} else if (ch == KEY_DOWN) {
+			erase(row, col);
+			row = row + 1;
+			attron(COLOR_PAIR(1));
+			mvaddch(row, col, main_char);
+			attroff(COLOR_PAIR(1));
+			refresh();
+		}
+		if (bomb[row][col] == true) {
+			Bomb a('h', rand() % 3 + 1);
+			bomb[row][col] = false;
+			defuse(a);
+			point++;
+			printBombs();
+			//break;
+		}
+		if (wall[row][col] == true) {
+			ulose(1);
+			break;
+		} else if (ch == 'q' || ch == 'Q') {
+			quit();
+			refresh();
+		}
+	}
+	return point;
 }
-int main(){
-    //define main character symbol and initial position
-    int y = 2, x = 2;
-    //char player = 'P';
+int main() {
+	//define main character symbol and initial position
+	int y = 2, x = 2;
+	//char player = 'P';
 	srand(time(NULL));
-    //printWorld();
-    //start ncurses
-    while(1){
-        for(int i=0; i<100; i++)
-            for(int j=0; j<100; j++){
-                wall[i][j]=0;
-                bomb[i][j]=0;
-                }
-                init();
-	 printw("Proceed to each bomb and defuse. Avoid all obstacles. For bomb type A cut RED wire. For bomb type B cut BLUE wire. For bomb type C cut GREEN wire.");
+	//printWorld();
+	//start ncurses
+	priority_queue<int> highScores;
+	while (1) {
+		for (int i = 0; i < 100; i++)
+			for (int j = 0; j < 100; j++) {
+				wall[i][j] = 0;
+				bomb[i][j] = 0;
+			}
+		init();
+		printw("Proceed to each bomb and defuse. Avoid all obstacles.\n");
+		printw("For bomb type A cut RED wire. For bomb type B cut BLUE wire. For bomb type C cut GREEN wire.\n");
+		printw("Press any key to continue.\n");
+		printw("Press Q anytime to quit.\n");
 
-	start_color();
-	init_pair(1,COLOR_MAGENTA,COLOR_BLACK);
-	init_pair(2,COLOR_RED,COLOR_BLACK);
-	init_pair(3,COLOR_WHITE,COLOR_BLACK);
-    int input=getch();
-    clear();
-    int score=gameLoop(y, x, input);
-   // endwin();
-    priority_queue<int> highScores;
-    highScores.push(score);
-    highScore(highScores.top());
-   // wall[100][100]={0};
-   // bomb[100][100]={0};
-    //highScores.pop();
-    endwin();
-    system("clear");
-    }
-    return 0;
+		start_color();
+		init_pair(1, COLOR_MAGENTA, COLOR_BLACK);
+		init_pair(2, COLOR_RED, COLOR_BLACK);
+		init_pair(3, COLOR_WHITE, COLOR_BLACK);
+		int input = getch();
+		if (input == 'q' || input == 'Q') {
+			quit();
+		}
+		clear();
+		int score = gameLoop(y, x, input);
+		highScores.push(score);
+		highScore(highScores.top());
+		// wall[100][100]={0};
+		// bomb[100][100]={0};
+		//highScores.pop();
+		endwin();
+		system("clear");
+	}
+	return 0;
 }
